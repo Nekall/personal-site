5acf7ccebf699b5caf0e982b9766db77c2186a6d
Neka <nekoanchor@yahoo.fr>
2021-10-05 14:02:00 +0200

    feat: add console easter egg

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,6 +20,9 @@ import { Link } from 'react-router-dom';
 
 const Home=()=>{
 
+  console.log(("  _   _               "), '\n', ("( ) ( )              "), '\n', ("| |_| |   __   _   _ "), '\n', ("|  _  | /'__`\\( ) ( )"), '\n',("| | | |(  ___/| (_) |"), '\n',("(_) (_)`\\____)`\\__, |"), '\n',("              ( )_| |"), '\n',("              `\\___/'"));
+  console.log("N'hésitez pas a me contacter si vous êtes interessés par mon profil ☺");
+
   return(
     <div className="container">
 
