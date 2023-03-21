5f31f80a44cd5af3c3681b6d1a2827042c063499
Neka <nekoanchor@yahoo.fr>
2023-03-21 17:27:11 +0100

    feat: update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+// Styles
+import styles from "./styles.module.scss";
 
 const Navbar=()=>(
-  <nav className="navbar">
+  <nav className={styles.navbar}>
     <div>
-      <h1 className="name">Benjamin Anezo</h1>
-      <p className="name front-name">Benjamin Anezo</p>
-      <p className="note">DEVELOPPEUR FULLSTACK JUNIOR</p>
+      <h1 className={styles.name}>Benjamin Anezo</h1>
+      <p className={`${styles.name} ${styles.front_name}`}>Benjamin Anezo</p>
+      <p className={styles.note}>DEVELOPPEUR FULLSTACK JUNIOR</p>
     </div>
-    <div className="anchors">
+    <div className={styles.anchors}>
       <a href="#competences">competences</a>
       <a href="#formation">formation</a>
       <a href="#projects">projets</a>
