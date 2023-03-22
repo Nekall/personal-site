c0c4cd495cb704076cc4b1c44c485ad2d904cff6
Neka <nekoanchor@yahoo.fr>
2023-03-22 20:33:08 +0100

    feat: update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+// Styles
+import styles from './styles.module.scss';
 
 const Footer=()=>(
-  <footer>
-    <div></div>
+  <footer className={styles.footer}>
+    <hr />
     <p>Site créé par Neka avec <span>♥</span></p>
   </footer>
 );
