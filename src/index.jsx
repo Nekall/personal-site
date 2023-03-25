bc2fe76a135a1b7f1f58f8b3d480d4e9b567dde8
Neka <nekoanchor@yahoo.fr>
2023-03-25 22:49:11 +0100

    refacto: remove dupli pages & opti with projects pages

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,16 @@ import Footer from 'components/Footer';
 import Name from 'components/Name';
 
 // Pages
-import Silverfish from 'pages/Silverfish';
-import Ronincode from 'pages/Ronincode';
-import Notacine from 'pages/Notacine';
 import Project from 'pages/Project';
-import Slickk from 'pages/Slickk';
-import Savime from 'pages/Savime';
 import Error from 'pages/Error';
 import Home from 'pages/Home';
+
+// Assets
+import silverfish from "./assets/images/projects/silverfish-screen.png";
+import ronincode from "./assets/images/projects/ronincode-screen.jpg";
+import notacine from "./assets/images/projects/notacine-screen.jpg";
+import slickk from "./assets/images/projects/slickk-screen.png";
+import savime from "./assets/images/projects/savime-screen.png";
 
 // Styles
 import './style/main.scss';
@@ -32,27 +34,23 @@ const App=()=>{
             </Route>
             <Route path="/ronincode" exact>
               <Name />
-              <Ronincode />
+              <Project name={"Projet RoninCode"} img={ronincode} alt={"ronincode"} link={"https://ronincode.vercel.app"} githubFullstack={"https://github.com/Nekall/ronincode"} description={"Plateforme web de mise en relation de mentors et d'apprentis en développement informatique proposant à ses membres des contenus et outils pédagogiques."} />
             </Route>
             <Route path="/notacine" exact>
               <Name />
-              <Notacine />
+              <Project name={"Projet Notacine"} img={notacine} alt={"notacine"} githubFullstack={"https://github.com/Day-101/NotaCine"} description={"Lorsque vous critiquez des films, vous devez prendre les évaluations très au sérieux. Cependant, nous pensons que la manière habituelle d'utiliser un chiffre pour évaluer un film est biaisée. C'est pourquoi nous avons créé NotaCine, un site web qui ne ressemble pas aux centaines d'autres services d'évaluation de films."} />
             </Route>
             <Route path="/silverfish-infesting-space" exact>
               <Name />
-              <Silverfish />
+              <Project name={"Projet Silverfish Infesting Space"} img={silverfish} alt={"silverfish"} link={"https://silverfish.infesting.space"} githubFullstack={"https://github.com/Nekall/Silverfish-Space"} description={"Site de communauté Minecraft, quelques pages de 'recettes' ajouté dans le jeu, et la possibilité de télécharger des textures personnalisées."} />
             </Route>
             <Route path="/slickk" exact>
               <Name />
-              <Slickk />
+              <Project name={"Slickk"} img={slickk} alt={"slickk"} link={"https://www.slickk.eu"} description={"Recrutez un Développeur Freelance à distance partout en Europe et constituez votre équipe en une journée seulement."} />
             </Route>
             <Route path="/savime" exact>
               <Name />
-              <Savime />
-            </Route>
-            <Route path="/project" exact>
-              <Name />
-              <Project />
+              <Project name={"Projet Savime"} img={savime} alt={"savime"} link={"https://www.savime.tech"} githubFrontend={"https://github.com/Nekall/Savime-front"} githubBackend={"https://github.com/Nekall/Savime-back"} description={"«The time-saver for teams & companies.» - Application orienté RH, Savime seɪv.ɪm sert à créer une communication rapide, simple & sécurisée entre des employé·es et le département RH d'une entreprise. Que se soit le partage de documents, la gestion des congés ou l’accès aux informations importantes lié à l’entreprise & la carrière des employé·es. Cette application se veux clef en main."} />
             </Route>
             <Route>
               <Name />
--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -7,7 +7,7 @@ import previous from 'assets/images/previous.svg';
 // Styles
 import styles from "../../style/pages/Project.module.scss";
 
-const Project = ({ name, img, alt, link,  githubBackend, githubFrontend, description}) => {
+const Project = ({ name, img, alt, link, githubBackend, githubFrontend, githubFullstack, description}) => {
     return (
         <div className="container">
         <Link to="/#projects">
@@ -15,8 +15,9 @@ const Project = ({ name, img, alt, link,  githubBackend, githubFrontend, descrip
         </Link>
         <p className="project-title">{name}</p>
         <img className={styles.image} src={img} alt={alt} />
-        {githubFrontend && <p><a href={githubFrontend.link} target="_blank" rel="noopener noreferrer">Github Frontend du projet</a></p>}
-        {githubBackend && <p><a href={githubBackend.link} target="_blank" rel="noopener noreferrer">Github Backend du projet</a></p>}
+        {githubFullstack && <p><a href={githubFullstack} target="_blank" rel="noopener noreferrer">Github du projet</a></p>}
+        {githubFrontend && <p><a href={githubFrontend} target="_blank" rel="noopener noreferrer">Github Frontend du projet</a></p>}
+        {githubBackend && <p><a href={githubBackend} target="_blank" rel="noopener noreferrer">Github Backend du projet</a></p>}
         {link && <p><a href={link} target="_blank" rel="noopener noreferrer">Visiter le site ici !</a></p>}
         <p className="project-description">{description}</p>
       </div>
