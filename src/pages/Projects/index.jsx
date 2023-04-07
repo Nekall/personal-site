94d73ae6b282f1f6fd73505a8e63610829a590d4
Neka <nekoanchor@yahoo.fr>
2023-04-07 23:15:50 +0200

    docs: add more informations about projects

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -19,27 +19,40 @@ const Projects = () => {
     {
       name: "Savime",
       description: "«The time-saver for teams & companies.»",
+      details: "«The time-saver for teams & companies.» - Application orienté RH, Savime seɪv.ɪm sert à créer une communication rapide, simple & sécurisée entre des employé·es et le département RH d'une entreprise. Que se soit le partage de documents, la gestion des congés ou l’accès aux informations importantes lié à l’entreprise & la carrière des employé·es. Cette application se veux clef en main.",
       image: savime,
+      github: "https://github.com/Nekall/Savime-front",
+      link: "https://www.savime.tech/"
     },
     {
       name: "Slickk",
       description: "Only Skills Matter",
+      details: "Recrutez un Développeur Freelance à distance partout en Europe et constituez votre équipe en une journée seulement.",
       image: slickk,
+      link: "https://www.slickk.eu/"
     },
     {
       name: "RoninCode",
       description: "Site mentorat et communauté de développeurs",
+      details: "Plateforme web de mise en relation de mentors et d'apprentis en développement informatique proposant à ses membres des contenus et outils pédagogiques.",
       image: ronincode,
+      github: "https://github.com/Nekall/ronincode",
+      link: "https://ronincode.vercel.app/"
     },
     {
       name: "NotaCine",
       description: "Site de notes de films",
+      details: "Lorsque vous critiquez des films, vous devez prendre les évaluations très au sérieux. Cependant, nous pensons que la manière habituelle d'utiliser un chiffre pour évaluer un film est biaisée. C'est pourquoi nous avons créé NotaCine, un site web qui ne ressemble pas aux centaines d'autres services d'évaluation de films.",
       image: notacine,
+      github: "https://github.com/Day-101/NotaCine"
     },
     {
       name: "SilverfishInfestingSpace",
       description: "Site de communauté Minecraft",
+      details: "Site de communauté Minecraft, quelques pages de 'recettes' ajouté dans le jeu, et la possibilité de télécharger des textures personnalisées.",
       image: silverfish,
+      github: "https://github.com/Nekall/Silverfish-Space",
+      link: "https://silverfish.infesting.space/"
     },
   ];
 
