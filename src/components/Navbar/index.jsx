f79b05bd1088b89f32fb86438464ef437b883e2b
Neka <nekoanchor@yahoo.fr>
2023-04-09 17:42:49 +0200

    feat: update responsive navbar & improve home page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,3 +1,10 @@
+// Assets
+import github from "../../assets/images/icons/github.svg";
+import twitter from "../../assets/images/icons/twitter.svg";
+import linkedin from "../../assets/images/icons/linkedin.svg";
+import codepen from "../../assets/images/icons/codepen.svg";
+import folder from "../../assets/images/icons/folder.svg";
+
 // Styles
 import styles from "./styles.module.scss";
 
@@ -9,24 +16,35 @@ const Navbar = () => {
       link: "/projects",
       target: "_self",
       rel: "",
-    },
-    {
-      name: "twitter",
-      link: "https://twitter.com/36Qm7p5CGz",
-      target: "_blank",
-      rel: "noopener noreferrer",
-    },
-    {
-      name: "codepen",
-      link: "https://codepen.io/LilNeka",
-      target: "_blank",
-      rel: "noopener noreferrer",
+      icon: folder
     },
     {
       name: "github",
       link: "https://github.com/Nekall",
       target: "_blank",
       rel: "noopener noreferrer",
+      icon: github
+    },
+    {
+      name: "codepen",
+      link: "https://codepen.io/LilNeka",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      icon: codepen
+    },
+    {
+      name: "twitter",
+      link: "https://twitter.com/36Qm7p5CGz",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      icon: twitter
+    },
+    {
+      name: "linkedin",
+      link: "https://www.linkedin.com/in/benjamin-anezo-40b83b216",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      icon: linkedin
     },
   ];
 
@@ -40,6 +58,15 @@ const Navbar = () => {
           <li key={`link-${index}`}>
             <a href={link} target={target} rel={rel}>
               {name}
+            </a>
+          </li>
+        ))}
+      </div>
+      <div className={styles.__mobile_links}>
+        {links.map(({ name, link, target, rel, icon }, index) => (
+          <li key={`link-${index}`}>
+            <a href={link} target={target} rel={rel}>
+              {icon ? <img src={icon} alt={name} /> : name}
             </a>
           </li>
         ))}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,22 +7,30 @@ const Home = () => {
   console.clear()
   console.log(("  _   _               "), '\n', ("( ) ( )              "), '\n', ("| |_| |   __   _   _ "), '\n', ("|  _  | /'__`\\( ) ( )"), '\n', ("| | | |(  ___/| (_) |"), '\n', ("(_) (_)`\\____)`\\__, |"), '\n', ("              ( )_| |"), '\n', ("              `\\___/'"));
   //console.log("N'hésitez pas a me contacter si vous êtes interessés par mon profil ☺");
-  
-  const experience = 
-  [
-    {
-      title: "Piscine de 42",
-      date: "2017",
-    },
-    {
-      title: "The Hacking Project",
-      date: "2021",
-    },
-    {
-      title: "3W Academy ✕ Slickk",
-      date: "2022-PRESENT",
-    },
-  ]
+
+  const experience =
+    [
+      {
+        title: "Piscine de 42",
+        date: "2017",
+        details: "Piscine de 42, 2 mois de formation intensive en C."
+      },
+      {
+        title: "Grande Distribution",
+        date: "2018-2020",
+        details: "Travail dans une grande surface, en tant qu'employé libre service."
+      },
+      {
+        title: "The Hacking Project",
+        date: "2021",
+        details: "Formation intensive de 6 mois en développement web."
+      },
+      {
+        title: "3W Academy ✕ Slickk",
+        date: "2022-PRESENT",
+        details: "Alternance de 2 années en développement web."
+      },
+    ]
 
   return (
     <div className={styles.__home}>
@@ -31,16 +39,23 @@ const Home = () => {
         <p>Cela fait des années que le monde du développement informatique m'attire. En 2021 j'ai enfin sauté le pas, en quittant mon ancien travail dans l'agroalimentaire et en rejoignant le bootcamp de <a href="https://www.thehackingproject.org/" target="_blank" rel="noopener noreferrer">The Hacking Project</a> afin de devenir fullstack développeur. Suite à cette première formation et dans le but de me professionnaliser, je suis actuellement étudiant à la <a href="https://3wa.fr/" target="_blank" rel="noopener noreferrer">3w Academy</a>  en alternance chez <a href="https://www.slickk.eu/" target="_blank" rel="noopener noreferrer">Slickk</a>.</p>
         <p>Je vis actuellement à Paris.</p>
         <div>
-        <div className={styles.__experience}>
-          <ol>
-            {experience.map(({ title, date }, index) => (
-              <li key={`exp-${index}`}>{title} <span>― {date}</span> </li>
-            ))}
-          </ol>
-        </div>
-        
-        <a className={styles.__btn_download} href="/Neka_Anezo_CV.png" target="_blank">Curriculum vitæ </a>
-        <p className={styles.__stamp}>2017-PRESENT, Have a nice dev ! ― Neka</p>
+          <div className={styles.__experience}>
+            <ol>
+              {experience.map(({ title, date, details }, index) => (
+                <li key={`exp-${index}`}>
+                  <details>
+                    <summary>
+                      {title} <span>― {date}</span>
+                    </summary>
+                    <p>{details}</p>
+                  </details>
+                </li>
+              ))}
+            </ol>
+          </div>
+
+          <a className={styles.__btn_download} href="/Neka_Anezo_CV.png" target="_blank">Curriculum vitæ </a>
+          <p className={styles.__stamp}>2017-PRESENT, Have a nice dev ! ― Neka</p>
         </div>
       </div>
     </div>
