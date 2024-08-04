d743df6917a707483ae5f275c4d61b470279cc54
Neka <neka.anezo@protonmail.com>
2024-08-05 00:17:26 +0200

    feat: add cl

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-console.log(window.location.href.split("/")[3]);
+//console.log(window.location.href.split("/")[3]);
+console.clear();
+console.log(
+  "  _   _               ",
+  "\n",
+  "( ) ( )              ",
+  "\n",
+  "| |_| |   __   _   _ ",
+  "\n",
+  "|  _  | /'__`\\( ) ( )",
+  "\n",
+  "| | | |(  ___/| (_) |",
+  "\n",
+  "(_) (_)`\\____)`\\__, |",
+  "\n",
+  "              ( )_| |",
+  "\n",
+  "              `\\___/'"
+);
+console.log(
+  "N'hésitez pas à me contacter si vous êtes intéressés par mon profil ☺"
+);
