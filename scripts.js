398edc482e068e9c10944f1139e8914eb1d08033
Neka <neka.anezo@protonmail.com>
2024-08-05 14:33:35 +0200

    feat: add projects listing

--- scripts.js
+++ scripts.js
@@ -20,3 +20,18 @@ console.log(
 console.log(
   "N'hésitez pas à me contacter si vous êtes intéressés par mon profil ☺"
 );
+
+/* Modal Projects */
+const projectsBtns = document.getElementsByClassName("project-btn");
+console.log(projectsBtns);
+
+const open = (id) => {
+  for (let i = 0; 0 < projectsBtns.length; i++) {
+    projectsBtns[i].setAttribute("id", id === i ? "open" : "");
+  }
+};
+
+for (let i = 0; i < projectsBtns.length; i++) {
+  console.log(i);
+  projectsBtns[i].addEventListener("click", () => open(i));
+}
